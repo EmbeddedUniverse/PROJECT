2a0449556e3b2fe1a6017f4aff4b824af59cd242
MoronixProduct3 <moronicproduct3@gmail.com>
2017-10-20 15:59:02 -0400

    Refactored autoCorrelation

--- Sources/C/mathematic.c
+++ Sources/C/mathematic.c
@@ -11,53 +11,22 @@
 
 float* autocorrelate(float* signal, int sizeInBytesSignal){
 
-    short nbBytesCorrArr = 2*sizeInBytesSignal-sizeof(float);
+    int N = sizeInBytesSignal / sizeof(float);
 
-    float*delayedSignal = (float*)malloc(sizeInBytesSignal);
-    int nbDataSignal = sizeInBytesSignal/sizeof(float);
+    int resultLength = 2 * N - 1;
 
-    float*corrArr = (float*)malloc(nbBytesCorrArr);
-  //  int nbDataCorrArr = nbBytesCorrArr/sizeof(float);
+    float * result = (float *) malloc(resultLength * sizeof(float));
 
-    short k;
-    int index;
-    int i,j,m;
+    for (int i = 0; i < resultLength-1; ++i)
+    {
+        int k = i - (N-1);
+        int start = (k > 0) ? k : 0;
+        int end = (k < 0) ? i : N-1;
+        int sum = 0;
 
-    for (k = -(nbDataSignal-1) ; k < nbDataSignal ; ++k){
-        index = 0;
-        if( k < 0 ){
-            for (i = -k ; i < nbDataSignal ; ++i){
-                delayedSignal[ index ] = signal[i];
-                ++index;
-            }
-
-            for (j = index ; j < nbDataSignal ; ++j){
-                delayedSignal[ index ] = 0.0;
-                ++index;
-            }
-        }
-        else if( k > 0 ){
-            for (j = 0 ; j < (k + 1) ; ++j){
-                delayedSignal[ index ] = 0.0;
-                ++index;
-            }
-
-            for (i = 0 ; i < nbDataSignal ; ++i){
-                delayedSignal[ index ] = signal[i];
-                ++index;
-            }
-
-        }
-        else if( k == 0 ){
-            for(i = 0 ; i < nbDataSignal ; ++i)
-                delayedSignal[i] = signal[i];
-        }
-
-        for (m = 0 ; m < nbDataSignal ; ++m){
-            corrArr[ nbDataSignal - 1 + k ] = corrArr[ nbDataSignal - 1 + k ] + signal[ m ] * delayedSignal[ m ];
-        }
+        for(int n = start; n <= end; ++n)
+            result[i] += signal[n] * signal[n-k];
     }
 
-
-    return corrArr;
+    return result;
 }
