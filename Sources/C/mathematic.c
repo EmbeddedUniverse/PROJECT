2925b83b3642f0eba3153fec865a6da2a67cdbba
MrParris <apaulparris@gmail.com>
2017-10-20 18:36:51 -0400

    autocorrelation code in c optimized, needs enhancing/comments

--- Sources/C/mathematic.c
+++ Sources/C/mathematic.c
@@ -11,53 +11,33 @@
 
 float* autocorrelate(float* signal, int sizeInBytesSignal){
 
-    short nbBytesCorrArr = 2*sizeInBytesSignal-sizeof(float);
-
-    float*delayedSignal = (float*)malloc(sizeInBytesSignal);
     int nbDataSignal = sizeInBytesSignal/sizeof(float);
 
+    short nbBytesCorrArr = 2*sizeInBytesSignal-sizeof(float);
     float*corrArr = (float*)malloc(nbBytesCorrArr);
-  //  int nbDataCorrArr = nbBytesCorrArr/sizeof(float);
 
-    short k;
-    int index;
-    int i,j,m;
-
+    short corrArrIndex, dSignalIndex, i, k, limit, delay;
+    limit = 0;
+    delay = 0;
     for (k = -(nbDataSignal-1) ; k < nbDataSignal ; ++k){
-        index = 0;
         if( k < 0 ){
-            for (i = -k ; i < nbDataSignal ; ++i){
-                delayedSignal[ index ] = signal[i];
-                ++index;
-            }
-
-            for (j = index ; j < nbDataSignal ; ++j){
-                delayedSignal[ index ] = 0.0;
-                ++index;
-            }
+            limit++;
+            dSignalIndex = -k;
         }
         else if( k > 0 ){
-            for (j = 0 ; j < (k + 1) ; ++j){
-                delayedSignal[ index ] = 0.0;
-                ++index;
-            }
-
-            for (i = 0 ; i < nbDataSignal ; ++i){
-                delayedSignal[ index ] = signal[i];
-                ++index;
-            }
-
+            limit--;
+            dSignalIndex = 0;
+            delay++;
         }
         else if( k == 0 ){
-            for(i = 0 ; i < nbDataSignal ; ++i)
-                delayedSignal[i] = signal[i];
+            limit++;
+            dSignalIndex = 0;
         }
-
-        for (m = 0 ; m < nbDataSignal ; ++m){
-            corrArr[ nbDataSignal - 1 + k ] = corrArr[ nbDataSignal - 1 + k ] + signal[ m ] * delayedSignal[ m ];
+        for (i = 0 ; i < limit ; ++i){
+            corrArr[corrArrIndex] = corrArr[corrArrIndex] + signal[i + delay] * signal[dSignalIndex];
+            dSignalIndex++;
         }
+        corrArrIndex++;
     }
-
-
     return corrArr;
 }
