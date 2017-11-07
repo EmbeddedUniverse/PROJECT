5faf69d29ee33798f29c9d4304a994136faf6665
valeriegauthier <valerie--6@hotmail.com>
2017-11-07 13:59:45 -0500

    Revert ".h pour correlation"
    This reverts commit fa513fbf3dcdf76a0f26c40e6c1f88df1115a781.

---
+++ Includes/correlation.h
@@ -0,0 +1,20 @@
+/*
+ * Holds the tests.
+ * correlation.h
+ *
+ */
+
+#ifndef CORRELATION_H_
+#define CORRELATION_H_
+
+#include <stdbool.h>
+#include <stdlib.h>
+#include <math.h>
+
+float* correlate(float* signalX, float* signalRef, int nbDataSignalX, int nbDataSignalRef);
+
+
+
+
+
+#endif
