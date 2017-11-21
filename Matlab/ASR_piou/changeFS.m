ce4c59643ec1d5818308cf0b64cfc6621870880f
MoronixProduct3 <moronicproduct3@gmail.com>
2017-11-21 15:44:45 -0500

    Removed dead code

--- Matlab/ASR_piou/changeFS.m
+++
@@ -1,8 +0,0 @@
-function [ adjustedSignal ] = changeFS( original, Fs, newFs )
-%CHANGEFS Summary of this function goes here
-%   Detailed explanation goes here
-[P,Q] = rat(newFs / Fs);
-adjustedSignal = resample(original, P, Q);
-
-end
-
