33a95c70bb953e2647ee1393bc310b50cda8e5a5
Édouard Denommée <dene2303@usherbrooke.ca>
2017-11-22 15:46:01 -0500

    Putting samples in dat file

--- Matlab/generateTestSignals.m
+++ Matlab/generateTestSignals.m
@@ -2,19 +2,43 @@ clc
 clear all
 close all
 
-goodSampleName = 'TEST_PEW_GOOD';
-badSampleName = 'TEST_PEW_BAD';
+addpath(genpath('voiceRecognition'));
 
-signalLength = 8192;
+goodSampleName = 'TEST_PEW_GOOD';
+nbGoodSampleName = 'NB_TEST_PEW_GOOD';
+badSampleName = 'TEST_PEW_BAD';
+nbBadSampleName = 'NB_TEST_PEW_BAD';
+nbBadSample = 1;
+
+sampleLengthName = 'TEST_SAMPLE_LENGTH';
+sampleLength = 8192;
+samplingFrequencyName = 'TEST_SAMPLE_FREQUENCY';
 samplingFrequency = 16000;
 
 % BadSignal
 load mtlb
-
-[signal, inputFs] = audioread('../Ressources/AudioSample/classicPew.wav');
-
-
-fsAdjustedGood = changeFS(signal, inputFs, samplingFrequency);
 fsAdjustedBad = changeFS(mtlb, Fs, samplingFrequency);
 
-alignedGood = alignSignal(fsAdjustedGood);
+% Good Signals
+[signals, inputFs] = loadAllWavSamples('../Ressources/AudioSample/');
+nbGoodSample = size(signals,2);
+fsAdjustedGood = zeros(nbGoodSample,sampleLength);
+
+for i = 1:nbGoodSample
+    x = changeFS( (signals(:, i))' , inputFs, samplingFrequency);
+    fsAdjustedGood(i,1:size(x,2)) = x;
+end
+
+% player = audioplayer(fsAdjustedGood(5,:), samplingFrequency);
+% play(player);
+
+fileID = fopen('../Ressources/TestVoiceSamples.dat','w');
+fprintf(fileID, '#define %s %d\r\n', sampleLengthName, sampleLength);
+fprintf(fileID, '#define %s %d\r\n', samplingFrequencyName, samplingFrequency);
+fprintf(fileID, '#define %s %d\r\n', nbGoodSampleName, nbGoodSample);
+fprintf(fileID, '#define %s %d\r\n', nbBadSampleName, nbBadSample);
+
+% Good samples
+fprintf(fileID, '\r\nfloat %s [%s][%s] = \r\n{\r\n', goodSampleName, nbGoodSampleName, sampleLengthName);
+
+fclose(fileID);
