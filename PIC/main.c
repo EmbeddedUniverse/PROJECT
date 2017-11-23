bbd1b4ee0a7de120bc59039650ccab37323fd19c
Unknown <philippe.marcoux2@usherbrooke.ca>
2017-11-23 12:09:20 -0500

    PIC : Config func and random target selector

--- PIC/main.c
+++ PIC/main.c
@@ -11,23 +11,58 @@
 #include <xc.h>            /* XC8 General Include File */
 #include <stdint.h>        /* For uint8_t definition */
 #include <stdbool.h>       /* For true/false definition */
-
 #include "usart.h" 
+#include <time.h>//required to use 'srand(time(NULL))'
 
 #define _XTAL_FREQ 8000000   // Oscillator frequency for _delay()
 #pragma config XINST = OFF 
 
 // Global variables
-bool rxFlag = false; // USART Data Received flag
 
+unsigned char rxChar = 0x00;
+unsigned char transferedData = 0x00;
+short nextTarget[2];
+bool ErrorUART = false; 
+bool rxFlag = false; // USART Data Received flag
 // Declarations
 void interrupt rxIsr(void);
+void setUARTconfig(void);
+void getRandomTarget(short Target[2]);
 
+// Main 
 void main(void) {
+    setUARTconfig();
     
-    // Variables
-    unsigned char rxChar = 0x00;
-    unsigned char transferedData = 0x00;
+    while(true){
+        if(rxFlag){
+            rxChar = RCREG1; // Read the value in the register
+            //if(rxChar==){
+            
+            //command
+            
+            }
+            rxFlag = false; // Reset the flag
+            PIE1bits.RC1IE = 1; // Re-enable the interrupt control
+            //TXREG1 = 0xf0;
+            
+        }
+    }
+}
+
+void interrupt rxIsr(void)
+{
+    if(PIR1bits.RC1IF && PIE1bits.RC1IE) {
+        if(RCSTAbits.FERR || RCSTAbits.OERR) {
+            ErrorUART= true;
+        } else {
+            rxFlag = true; // Raise the flag
+            PIE1bits.RC1IE = 0; // Disable the interrupt control
+        }
+    }
+}
+
+void setUARTconfig(void){
+ // configuration des PIN UART
     RPINR0_1 =   0b01100110; 
     RPOR26_27 =  0b00010001;
     // Initial register configuration for UART
@@ -46,30 +81,9 @@ void main(void) {
     
     // Send initial signal & console clear
     TXREG1 = 0xAA;
-    
-    while(true){
-        
-        if(rxFlag){
-            rxChar = RCREG1; // Read the value in the register
-            rxFlag = false; // Reset the flag
-            PIE1bits.RC1IE = 1; // Re-enable the interrupt control
-            TXREG1 = 0xf0;
-            
-        }
-    }
 }
 
-void interrupt rxIsr(void)
-{
-    if(PIR1bits.RC1IF && PIE1bits.RC1IE) {
-        if(RCSTAbits.FERR || RCSTAbits.OERR) {
-          //  TXREG1 = 0x45; // Send E (Error)
-        } else {
-            rxFlag = true; // Raise the flag
-            PIE1bits.RC1IE = 0; // Disable the interrupt control
-        }
-    }
+void getRandomTarget(short Target[2]){
+    Target[0] = rand() % 6 + 1;
+    Target[1] = rand() % 2 + 1;
 }
-
-
-
