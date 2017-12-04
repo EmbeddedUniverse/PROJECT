93591b5553ada241ab3b9f0a61ab4e2bf73c68d2
Anthony Larivière <lara2117@usherbrooke.ca>
2017-12-04 13:30:32 -0500

    Added test program for PIC debugging

--- Sources/C/run.c
+++ Sources/C/run.c
@@ -8,46 +8,24 @@
 #include <stdio.h>
 #include <dsk6713.h>
 #include <dsk6713_led.h>
-#include "voiceDetector.h"
-#include "piouRecognition.h"
+#include "communication.h"
 
 void run(){
 
-    printf("\n----------------------------------------------\n");
-    printf("\t    RUNNING PROGRAM\n");
-    printf("----------------------------------------------\n");
 
     DSK6713_init();
-    DSK6713_LED_init();
-    int i;
-    for (i = 0; i< 4; ++i)
-        DSK6713_LED_off(i);
+    COM_init();
 
-    VOICE_init();
-    PIOU_init();
 
     while(1)
     {
-        if (voiceSampleReady)
-        {
-            //play(voiceSample, VOICE_BUFFER_LENGTH);
-            //while(!readyToPlay);
+        unsigned char command;
 
-            if (detectPiou(voiceSample))
-            {
-                DSK6713_LED_on(0);
-                DSK6713_LED_on(1);
-                DSK6713_LED_on(2);
-                DSK6713_LED_on(3);
-                DSK6713_waitusec(125000);
-                DSK6713_LED_off(0);
-                DSK6713_LED_off(1);
-                DSK6713_LED_off(2);
-                DSK6713_LED_off(3);
-            }
+        printf("Type hex code to send:\n");
 
-            VOICE_reset();
-        }
+        scanf("%x", &command);
+
+        COM_send(command, PIC);
     }
 }
 
