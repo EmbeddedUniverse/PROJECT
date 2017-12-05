7933630af99ba9a322dd1fba54fd42bbfa472621
Unknown <philippe.marcoux2@usherbrooke.ca>
2017-12-04 21:21:41 -0500

    Menu Added

---
+++ Includes/Menu.h
@@ -0,0 +1,19 @@
+
+
+#include <stdio.h>
+#include <stdbool.h>
+#include "communication.h"
+
+#ifndef INCLUDES_MENU_H_
+#define INCLUDES_MENU_H_
+
+
+
+void runMenu();
+void choisirNbrBalle();
+void choisrTemps();
+void instruction();
+
+
+
+#endif /* INCLUDES_RUN_H_ */
